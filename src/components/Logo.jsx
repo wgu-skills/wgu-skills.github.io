f9d2cfa3310f5ffca9b67bee0c963ceaebdc02ad
David Petersen <dp@davidp.us>
2024-03-21 11:16:33 -0600

    Fix class name in Logo component and update Navbar styling

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 const Logo = () => {
     return (
         <a href='./'>
-            <span class="sr-only">Homepage</span>
+            <span className="sr-only">Homepage</span>
             <img src="./logo-light.svg" alt="Western Governors University" aria-hidden="true" className="hidden dark:block pr-4 p-0 m-0 leading-6 border-0 cursor-pointer text-stone-900" width={110} height={24}/>
             <img src="./logo-dark.svg"  alt="Western Governors University" aria-hidden="true" className="dark:hidden pr-4 p-0 m-0 leading-6 border-0 cursor-pointer text-stone-900" width={110} height={24}/>
         </a>
